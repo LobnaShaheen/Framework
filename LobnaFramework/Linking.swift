0d751bd6ac263886ec81a2821b52e8304f347da0
Lobna Shaheen <lobna@waffarx.com>
2022-12-13 19:24:04 +0200

    Add Linking File

---
+++ LobnaFramework/Linking.swift
@@ -0,0 +1,42 @@
+//
+//  Linking.swift
+//  LobnaFramework
+//
+//  Created by Lobna Shaheen on 13/12/2022.
+//
+
+import Foundation
+
+class Linking {
+    private let defaults = UserDefaults.standard
+    
+    public init(){}
+    
+    public func saveShoppingTrip(url: URL){
+        guard let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true) else {
+            return
+        }
+        guard let items = urlComponents.queryItems else {return}
+        var queryItems: [String : String] = [:]
+        for item in items {
+            queryItems[item.name] = item.value?.removingPercentEncoding
+            if(item.name == "shoppingTrip")
+            {
+                print(queryItems[item.name]!)
+                defaults.set(queryItems[item.name]!, forKey: "ShoppintTripID")
+                
+            }
+        }
+        if #available(iOS 15, *) {
+            defaults.set(Date.now, forKey: "ShoppingTripDate")
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+    public func endShoppingTrip(){
+        let shoppintTripID = defaults.integer(forKey: "ShoppintTripID")
+        let shoppingTripDate = defaults.object(forKey: "ShoppingTripDate") as! Date
+        
+        // MARK: CALL API HERE
+    }
+}
